b13aaa2c1fd7a69e991cb47a239f9e6658ff1d27
Sophie1142 <43194894+Sophie1142@users.noreply.github.com>
2020-09-29 19:48:08 -0400

    Update gif.c

---
+++ distribution/gif.c
@@ -0,0 +1,4 @@
+// Write a program gif.c that checks if a file (likely) a GIF (specifically, 
+// a GIF 89a file, the latest version of the GIF file format). 
+// Note that the first six characters of a GIF 89a file are the 
+// characters G, I, F, 8, 9, a.

713c2afe4419451f6423abcf292c78fec2e17b3b
Sophie Edouard <sedouard@college.harvard.edu>
2020-09-29 06:16:22 +0000

    update concat

---
+++ concat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s1 = get_string("s1: ");
+    char *s2 = get_string("s2: ");
+
+    char *s3 = concatenate(s1, s2);
+    printf("%s\n", s3);
+    free(s3);
+}
+
+// TODO: Implement concatenate function
